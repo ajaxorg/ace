addebed77c5fb007886b053ec7b97a696e84c386
Fabian Jakobs <fabian.jakobs@web.de>
2010-05-06 17:06:59 +0200

    add more V8 debugger commands

--- src/test/ace/assertions.js
+++ src/test/ace/assertions.js
@@ -2,3 +2,7 @@ assertPosition = function(row, column, cursor) {
     assertEquals(row, cursor.row);
     assertEquals(column, cursor.column);
 };
+
+assertJsonEquals = function(expectedJson, foundJson) {
+    assertEquals(JSON.stringify(expectedJson), JSON.stringify(foundJson));
+};
