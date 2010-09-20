1d4c6fea49fd90915bcdb217dfdc87df3a74d4c6
Fabian Jakobs <fabian.jakobs@web.de>
2010-09-20 09:08:09 +0200

    optimize scrolling by reusing already rendered lines

--- src/ace/VirtualRenderer.js
+++ src/ace/VirtualRenderer.js
@@ -194,7 +194,7 @@ var VirtualRenderer = function(container) {
         this.$textLayer.updateLines(layerConfig, firstRow, lastRow);
     };
 
-    this.draw = function() {
+    this.draw = function(scrollOnly) {
         var lines = this.lines;
 
         var offset = this.scrollTop % this.lineHeight;
@@ -216,7 +216,8 @@ var VirtualRenderer = function(container) {
             lastRow : lastRow,
             lineHeight : this.lineHeight,
             characterWidth : this.characterWidth,
-            minHeight : minHeight
+            minHeight : minHeight,
+            scrollOnly: !!scrollOnly
         };
 
         this.content.style.marginTop = (-offset) + "px";
@@ -309,7 +310,7 @@ var VirtualRenderer = function(container) {
         if (this.scrollTop !== scrollTop) {
             this.scrollTop = scrollTop;
             this.$updateScrollBar();
-            this.draw();
+            this.draw(true);
         }
     };
 
