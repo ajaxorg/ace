7d62c76128954184d001bdc91bd927136ef0f602
Fabian Jakobs <fabian.jakobs@web.de>
2010-10-25 16:39:48 +0200

    the cursor works always in document space

--- src/ace/Document.js
+++ src/ace/Document.js
@@ -259,14 +259,6 @@ var Document = function(text, mode) {
         return this.lines[row] || "";
     };
     
-    /**
-     * Get a line as it is displayed on screen. Tabs are replaced by spaces.
-     */
-    this.getDisplayLine = function(row) {
-        var tab = new Array(this.getTabSize()+1).join(" ");
-        return this.lines[row].replace(/\t/g, tab);
-    };
-
     this.getLines = function(firstRow, lastRow) {
         return this.lines.slice(firstRow, lastRow+1);
     };
