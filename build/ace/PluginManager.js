fe57908671362bb837bd8ddd34f38371bdafb361
Fabian Jakobs <fabian.jakobs@web.de>
2010-11-09 09:52:23 +0100

    temporarily remove build

--- build/ace/PluginManager.js
+++
@@ -1,8 +0,0 @@
-/*
- LGPLv3 <http://www.gnu.org/licenses/lgpl-3.0.txt>
-*/
-require.def("ace/PluginManager", [], function() {
-  return{commands:{}, registerCommand:function(a, b) {
-    this.commands[a] = b
-  }}
-});
--- build/ace/test/all.js
+++
@@ -1,4 +0,0 @@
-require({paths:{ace:"../src/ace"}}, ["ace/test/assertions", "ace/test/ChangeDocumentTest"], function(a) {
-  console.log(a);
-  alert("a " + a)
-});
