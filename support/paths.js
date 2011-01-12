3a892c7207a5b66d4716dd7eeffb286fb440c3ee
Fabian Jakobs <fabian.jakobs@web.de>
2011-01-12 15:33:42 +0100

    fix tests

--- support/paths.js
+++ support/paths.js
@@ -1,6 +1,7 @@
 require("./requireJS-node");
 require.paths.unshift(__dirname + "/../lib");
-require.paths.unshift(__dirname + "/../plugins");
+require.paths.unshift(__dirname + "/cockpit/lib");
+require.paths.unshift(__dirname + "/cockpit/support/pilot/lib");
 require.paths.unshift(__dirname + "/async/lib");
 require.paths.unshift(__dirname + "/node-htmlparser/lib");
 require.paths.unshift(__dirname + "/jsdom/lib");
