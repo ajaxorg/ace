2468259160d92f5b91d3e7a133fa0eba6af26406
Fabian Jakobs <fabian.jakobs@web.de>
2011-01-14 15:07:06 +0100

    remove old build

--- build/cockpit/index.js
+++
@@ -1,9 +0,0 @@
-define(function(a, d) {
-  d.startup = function(b, c) {
-    a("pilot/index");
-    a("cockpit/cli").startup(b, c);
-    window.testCli = a("cockpit/test/testCli");
-    a("cockpit/ui/settings").startup(b, c);
-    a("cockpit/ui/cliView").startup(b, c)
-  }
-});
--- build/pilot/commands/history.js
+++
@@ -1,6 +0,0 @@
-define(function(c, a) {
-  var b = 0;
-  a.addedRequestOutput = function() {
-    b = history.requests.length
-  }
-});
