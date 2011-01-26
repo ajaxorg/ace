e28a49f8f0a5dd8a0e7d69783fc6baf353435fee
Julian Viereck <julian.viereck@gmail.com>
2011-01-27 00:07:30 +0100

    Get tests up and running again

--- support/paths.js
+++ support/paths.js
@@ -1,4 +1,6 @@
 require("./requireJS-node");
 require.paths.unshift(__dirname + "/../lib");
 require.paths.unshift(__dirname + "/pilot/lib");
+require.paths.unshift(__dirname + "/async/lib");
+require.paths.unshift(__dirname + "/jsdom/lib");
 require.paths.unshift(__dirname);
