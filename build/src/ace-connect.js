fb47447dfefd94923aeb05c6b563598cf9b2c0d1
aslakhellesoy <aslak.hellesoy@gmail.com>
2011-04-21 22:54:38 +0100

    Connect middleware for serving ace from a node.js app. Also added ace-connect npm package for easy install.

---
+++ build/src/ace-connect.js
@@ -0,0 +1,28 @@
+var connect = require('connect');
+
+/**
+ * Connect middleware that serves the Ajax.org Code Editor (Ace).
+ *
+ * Example:
+ *
+ *     var connect = require('connect');
+ *     var ace = require('ace-connect')('/js/ace');
+ *     var server = connect.createServer(ace);
+ *     server.listen(3000);
+ *
+ * This will serve ace.js under http://localhost:3000/js/ace/ace.js
+ *
+ * @param path the path under which the ace files will be mounted.
+ */
+module.exports = function(path) {
+  var aceFiles = connect.static(__dirname);
+
+  return function(req, res, next) {
+    if(req.url.indexOf(path) == 0) {
+      req.url = req.url.slice(path.length);
+      aceFiles(req, res, next);
+    } else {
+      next();
+    }
+  };
+};
