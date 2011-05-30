2d45f9d5b5e4fa1ff91821e8a64372fc364899b3
Fabian Jakobs <fabian.jakobs@web.de>
2011-05-30 11:38:23 +0200

    update packaged ace

--- build/src/ace-uncompressed.js
+++ build/src/ace-uncompressed.js
@@ -3417,6 +3417,22 @@ exports.copyArray = function(array){
     return copy;
 };
 
+exports.deepCopy = function (obj) {
+    if (typeof obj[key] != "object") {
+        return obj;
+    }
+    
+    var copy = obj.constructor();
+    for (var key in obj) {
+        if (typeof obj[key] == "object") {
+            copy[key] = this.deepCopy(obj[key]);
+        } else {
+            copy[key] = obj[key];
+        }
+    }
+    return copy;
+}
+
 exports.arrayToMap = function(arr) {
     var map = {};
     for (var i=0; i<arr.length; i++) {
