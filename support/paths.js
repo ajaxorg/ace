da0113ba9834ec64fceea35efb83304539b7c0db
Fabian Jakobs <fabian.jakobs@web.de>
2011-07-11 12:10:02 +0200

    move dryice from npm dep to submodule dep

--- support/paths.js
+++ support/paths.js
@@ -1,6 +1,7 @@
 require("./requireJS-node");
 require.paths.unshift(__dirname + "/../lib");
 require.paths.unshift(__dirname + "/pilot/lib");
+require.paths.unshift(__dirname + "/dryice/lib");
 require.paths.unshift(__dirname + "/async/lib");
 require.paths.unshift(__dirname + "/jsdom/lib");
 require.paths.unshift(__dirname);
