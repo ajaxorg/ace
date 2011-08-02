213cffdffbf168e398cf594808c963bd39f404ef
Fabian Jakobs <fabian.jakobs@web.de>
2011-08-02 11:50:42 +0200

    add missing language modes to the build script.
    fix #316

--- Makefile.dryice.js
+++ Makefile.dryice.js
@@ -201,7 +201,8 @@ console.log('# ace modes ---------');
 project.assumeAllFilesLoaded();
 [
     "css", "html", "javascript", "php", "python", "xml", "ruby", "java", "c_cpp",
-    "coffee", "perl", "csharp", "svg", "clojure", "scss", "json"
+    "coffee", "perl", "csharp", "svg", "clojure", "scss", "json", "groovy",
+    "ocaml", "scala", "textile"
 ].forEach(function(mode) {
     console.log("mode " + mode);
     copy({
