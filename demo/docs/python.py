0c956185e3fca72b8e77a1e07f4e0dc7af06a722
Fabian Jakobs <fabian.jakobs@web.de>
2011-08-18 12:37:51 +0200

    outdent python after 'pass', 'raise', 'return', 'break' and 'continue'
    fix #382

--- demo/docs/python.py
+++ demo/docs/python.py
@@ -4,15 +4,16 @@ import string, sys
 
 # If no arguments were given, print a helpful message
 if len(sys.argv)==1:
-print 'Usage: celsius temp1 temp2 ...'
+print '''Usage:
+    celsius temp1 temp2 ...'''
 sys.exit(0)
 
 # Loop over the arguments
 for i in sys.argv[1:]:
-try:
-    fahrenheit=float(string.atoi(i))
-except string.atoi_error:
-    print repr(i), "not a numeric value"
-else:
-    celsius=(fahrenheit-32)*5.0/9.0
-    print '%i\260F = %i\260C' % (int(fahrenheit), int(celsius+.5))
+    try:
+        fahrenheit=float(string.atoi(i))
+    except string.atoi_error:
+        print repr(i), "not a numeric value"
+    else:
+        celsius=(fahrenheit-32)*5.0/9.0
+        print '%i\260F = %i\260C' % (int(fahrenheit), int(celsius+.5))
