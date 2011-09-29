65afcb20a3919e9d2f8cbdf1a31c8b266047db04
John Kane <john.kane84@gmail.com>
2011-09-30 00:41:45 +0100

    Added support for binary operators.

--- demo/docs/powershell.ps1
+++ demo/docs/powershell.ps1
@@ -3,6 +3,16 @@ function Hello($name) {
   Write-host "Hello $name"
 }
 
+function add($left, $right=4) {
+    if ($right -ne 4) {
+        return $left
+    } elseif ($left -eq $right) {
+        return 3
+    } else {
+        return 2
+    }
+}
+
 $number = 1 + 2;
 $number += 3
 
@@ -12,4 +22,3 @@ $an_array = @(1, 2, 3)
 $a_hash = @{"something" = "something else"}
 
 & notepad .\readme.md
-
