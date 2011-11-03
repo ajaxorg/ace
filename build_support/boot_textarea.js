065e371798b83368790dfadf129c3fe73aa334b1
Jason O'Neil <jason.oneil@gmail.com>
2011-11-03 13:48:16 +0800

    Changes to Textarea also

--- build_support/boot_textarea.js
+++ build_support/boot_textarea.js
@@ -425,6 +425,7 @@ function setupSettingPanel(settingDiv, settingOpener, api, options) {
             clojure:    "Clojure",
             ocaml:      "OCaml",
             csharp:     "C#",
+            haxe:       "haXe",
             svg:        "SVG",
             textile:    "Textile",
             groovy:     "Groovy",
