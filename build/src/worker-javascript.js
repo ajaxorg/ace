ea4d070bfd027a2e871b88304e37655236218c86
Fabian Jakobs <fabian.jakobs@web.de>
2011-11-10 10:35:43 +0100

    package

--- build/src/worker-javascript.js
+++ build/src/worker-javascript.js
@@ -1490,6 +1490,43 @@ exports.implement = function(proto, mixin) {
 };
 
 });
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Ajax.org Code Editor (ACE).
+ *
+ * The Initial Developer of the Original Code is
+ * Ajax.org B.V.
+ * Portions created by the Initial Developer are Copyright (C) 2010
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *      Fabian Jakobs <fabian AT ajax DOT org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+ 
 define('ace/mode/javascript_worker', ['require', 'exports', 'module' , 'ace/lib/oop', 'ace/worker/mirror', 'ace/worker/jshint', 'ace/narcissus/jsparse'], function(require, exports, module) {
     
 var oop = require("../lib/oop");
@@ -8448,7 +8485,7 @@ define('ace/narcissus/jsparse', ['require', 'exports', 'module' , 'ace/narcissus
         return n;
     }
 
-    return {
+    module.exports = {
         parse: parse,
         parseStdin: parseStdin,
         Node: Node,
@@ -9008,7 +9045,7 @@ define('ace/narcissus/jsparse', ['require', 'exports', 'module' , 'ace/narcissus
     };
 
 
-    return { Tokenizer: Tokenizer };
+    module.exports = { Tokenizer: Tokenizer };
 
 });
 /* vim: set sw=4 ts=4 et tw=78: */
