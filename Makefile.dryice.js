b229566f3cfc92a87f00d2c4c687754b78318fb4
Bernie Telles <btelles@gmail.com>
2011-11-15 19:04:13 -0800

    Add chrome theme to compile settings.

--- Makefile.dryice.js
+++ Makefile.dryice.js
@@ -244,7 +244,7 @@ project.assumeAllFilesLoaded();
 console.log('# ace themes ---------');
 
 [
-    "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn", "eclipse",
+    "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn", "eclipse",
     "idle_fingers", "kr_theme", "merbivore", "merbivore_soft",
     "mono_industrial", "monokai", "pastel_on_dark", "solarized_dark",
     "solarized_light", "textmate", "tomorrow", "tomorrow_night",
