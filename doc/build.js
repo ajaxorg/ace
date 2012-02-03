79886c3e07bfaf057ed7e17b07c31e1244ebdc43
Garen Torikian <gjtorikian@gmail.com>
2012-02-02 17:00:18 -0800

    First dump of doc stuff

---
+++ doc/build.js
@@ -0,0 +1,15 @@
+require("colors");
+
+var argv = require("optimist").argv,
+    ndoc = require(argv._[0] || "./build/ndoc/bin/ndoc");
+
+console.log("GENERATING DOCUMENTATION".green);
+
+var srcPath = process.cwd() + "/../lib/ace";
+
+ndoc.main(["--path=" + srcPath, "-o", "./out/", "-t", "ACE API", "--skin", "./resources/ace/skins"], function(err) {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+   }
+});
