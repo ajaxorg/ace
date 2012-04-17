23f74a3be3d025c44836665e3877a670fbea95c4
Garen Torikian <gjtorikian@gmail.com>
2012-04-17 11:02:08 -0700

    Doc API updates...almost ready

--- doc/build.js
+++ doc/build.js
@@ -1,11 +1,8 @@
-var argv = require("optimist").argv,
-    ndoc = require(argv._[0] || "./build/ndoc/bin/ndoc");
-
-console.log("GENERATING DOCUMENTATION");
+var panino = require("panino");
 
 var srcPath = process.cwd() + "/../lib/ace";
 
-ndoc.main(["--path=" + srcPath, "-o", "./out/", "-t", "ACE API", "--skin", "./resources/ace/skins"], function(err) {
+panino.main(["--path=" + srcPath, "-o", "./out/", "-r", "-t", "ACE API", "--skin", "./resources/ace/skins"], function(err) {
     if (err) {
         console.error(err);
         process.exit(-1);
