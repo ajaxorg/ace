512bde6fd8c40ace281a37691ea8ec569444606c
Garen Torikian <gjtorikian@gmail.com>
2012-04-21 17:54:46 -0700

    We're getting there

--- doc/resources/ace/skeleton/javascripts/clicker.js
+++ doc/resources/ace/skeleton/javascripts/clicker.js
@@ -1,41 +1,4 @@
 $(function() {
-//  $(".sideToggler").collapse();
-
-//  var documentURL = location.href;
-//  var lastSlashPos = documentURL.lastIndexOf("/") + 1;
-//  var filenamePos = documentURL.lastIndexOf(".html");
-
-//  var objName = documentURL.substring(lastSlashPos, filenamePos);
-
-//  var menuUl = "menu_" + objName;
-
-  // resize the sidebar/content based on window height;
-  // this enables us to have "inner" scroll bars
-   /*var h = $(window).height();
-   $("#sidebar").css('height', h - 80);
-   $("#content").css('height', h - 100);
-
-    $(window).resize(function(){
-        var h = $(window).height();
-        var w = $(window).width();
-        $("#sidebar").css('height', h - 80);
-        $("#content").css('height', h - 100);
-    }); */
-
-  // clicking on the member title should launch open the 
-  // description
-//  $('.member-name').click(function()  {
-//    var id = this.id.substring(this.id.indexOf("_") + 1);
-//
-//    $("h3[id='" + id + "']").trigger('click');
-//  });
-
-  /*$('.memberLink').click(function()  {
-    var id = this.id.substring(this.id.indexOf("_") + 1);
-
-    $("h3[data-id='" + id + "']").trigger('click');
-  }); */
-
   // when hovering over arrow, add highlight (only if inactive)
   $("h3.methodToggle").hover(function () {
       if (!$("h3.methodToggle").hasClass('active'))
