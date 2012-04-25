782da7637b5b3dee9bd3cc97126d9835bab2272a
Garen Torikian <gjtorikian@gmail.com>
2012-04-25 15:30:18 -0700

    Continue finalizing ace api doc

--- doc/resources/ace/skeleton/javascripts/ux.js
+++ doc/resources/ace/skeleton/javascripts/ux.js
@@ -60,6 +60,40 @@ $(document).ready(function () {
         });
     }
 
+	// force a sticky footer
+	$(window).bind("load", function() { 
+	
+	       var footerHeight = 0,
+	           footerTop = 0,
+	           $footer = $("#footer");
+	
+	       positionFooter();
+	
+	       function positionFooter() {
+	
+	                footerHeight = $footer.height();
+	                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+	
+	               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+	                   $footer.css({
+	                        position: "absolute"
+	                   }).animate({
+	                        top: footerTop
+	                   })
+	               } else {
+	                   $footer.css({
+	                        position: "static"
+	                   })
+	               }
+	
+	       }
+	
+	       $(window)
+	               .scroll(positionFooter)
+	               .resize(positionFooter)
+	
+	});
+
     var s, sx;
 
     // scrolling offset calculation via www.quirksmode.org
