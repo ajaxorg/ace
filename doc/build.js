1846f3c9ba075ebf3c943606396e551355438ff6
Garen Torikian <gjtorikian@gmail.com>
2012-08-24 11:14:23 -0700

    Update to new panino, fix doc bugs

--- doc/build.js
+++ doc/build.js
@@ -1,10 +1,38 @@
+var path = require("path");
+
 var panino = require("panino");
 
-var srcPath = process.cwd() + "/../lib/ace";
+var srcPath = __dirname + "/../lib/ace";
 
-panino.main(["--path=" + srcPath, "-o", "../api/", "-u", "../api/resources", "-a", "./additionalObjs.json", "-i", "index.md", "-t", "Ace API", "--skin", "./resources/ace/", "-s"], function(err) {
+var buildOptions = {
+	parseOptions: "./parseOptions.json",
+	additionalObjs: "./additionalObjs.json"
+}
+
+var options = {
+  title       : "Ace API",
+  linkFormat  : 'http://example.com/{file}#{line}',
+  output      : "../api/",
+  outputAssets : "../api/resources",
+  skin        : "./resources/ace/templates/layout.jade",
+  assets      : "./resources/ace/skeleton",
+  additionalObjs : "./additionalObjs.json",
+  exclude     : ["**/*_test.js", "**/mode/**", "**/test/**", "**/theme/**", "**/worker/**"],
+  index       : "./index.md"
+};
+
+files = [srcPath];
+
+panino.parse(files, options, function (err, ast) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  panino.render('html', ast, options, function (err) {
     if (err) {
-        console.error(err);
-        process.exit(-1);
-   }
+      console.error(err);
+      process.exit(1);
+    }
+  });
 });
