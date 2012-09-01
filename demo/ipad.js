f6e9cce4287d5f03ceb3d7cc0097178015a1f519
nightwing <amirjanyan@gmail.com>
2012-09-01 18:16:00 +0400

    update ipad demo

---
+++ demo/ipad.js
@@ -0,0 +1,72 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Mozilla Skywriter.
+ *
+ * The Initial Developer of the Original Code is
+ * Mozilla.
+ * Portions created by the Initial Developer are Copyright (C) 2009
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *      Fabian Jakobs <fabian AT ajax DOT org>
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+define(function(require, exports, module) {
+    
+var Dom = require("ace/lib/dom");
+var Event = require("ace/lib/event");
+
+var Editor = require("ace/editor").Editor;
+var EditSession = require("ace/edit_session").EditSession;
+var UndoManager = require("ace/undomanager").UndoManager;
+var Renderer = require("ace/virtual_renderer").VirtualRenderer;
+var JavaScriptMode = require("ace/mode/javascript").Mode;
+
+exports.launch = function(el, text) {
+    if (typeof(el) == "string") {
+        el = document.getElementById(el);
+    }
+    
+    editor = new Editor(new Renderer(el, "ace/theme/twilight"));
+    
+    var doc = new EditSession(text);
+    doc.setMode(new JavaScriptMode());
+    doc.setUndoManager(new UndoManager());
+    editor.setSession(doc);
+    
+    editor.resize();
+
+    /* setTimeout(function(){
+		editor.setAnimatedScroll(true)
+		editor.renderer.scrollToLine(editor.renderer.scrollTop == 0 ? 200 :0 ,0,true)	
+	}, 2000);*/
+	
+    return editor;
+};
+
+});
