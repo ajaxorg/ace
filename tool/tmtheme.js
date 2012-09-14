7ffe0a515300a5cb6551daf7524460e21dcd440d
Garen Torikian <gjtorikian@gmail.com>
2012-09-13 23:22:46 -0700

    Revert textmate theme to original

--- tool/tmtheme.js
+++ tool/tmtheme.js
@@ -220,7 +220,7 @@ var themes = {
     "pastel_on_dark": "Pastels on Dark",
     "solarized_dark": "Solarized-dark",
     "solarized_light": "Solarized-light",
-    "textmate": "Textmate (Mac Classic)",
+    //"textmate": "Textmate (Mac Classic)",
     "tomorrow": "Tomorrow",
     "tomorrow_night": "Tomorrow-Night",
     "tomorrow_night_blue": "Tomorrow-Night-Blue",
