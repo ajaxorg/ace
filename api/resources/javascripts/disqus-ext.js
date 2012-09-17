844c37fda935d819d1e1f24c2a638b93a20f7c78
Garen Torikian <gjtorikian@gmail.com>
2012-09-17 12:36:40 -0700

    Few more tweaks

--- api/resources/javascripts/disqus-ext.js
+++ api/resources/javascripts/disqus-ext.js
@@ -9,9 +9,9 @@ function setupDisqus(href) {
     
     (function() {
         if (document.getElementById("disqusScript") === null) {
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
             dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);       
         }
     })();
 }
