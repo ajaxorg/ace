4bbf4e7ac004408427e16ee733f61a6814c6d6b2
nightwing <amirjanyan@gmail.com>
2013-02-06 19:13:06 +0400

    Fixed: test case for scheme

--- demo/kitchen-sink/docs/scheme.scm
+++ demo/kitchen-sink/docs/scheme.scm
@@ -3,14 +3,11 @@
     for CD"
    (prompt-read "Title" 1.53 1 2/4 1.7 1.7e0 2.9E-4 +42 -7 #b001 #b001/100 #o777 #O777 #xabc55 #c(0 -5.6))
    (prompt-read "Artist")
-   (or (parse-integer (prompt-read "Rating") #:junk-allowed t) 0)
+   (or (parse-integer (prompt-read "Rating") #:junk-allowed #t) 0)
   (if x (format #t "yes") (format #f "no") ;and here comment
   ) 
   ;; second line comment
   '(+ 1 2)
-  (define-syntax get-line
-    (syntax-rules ()
-      ((_ x) x)))                ; nonsense, just a highlight test
   (position-if-not char-set:whitespace line #:start beg))
   (quote (privet 1 2 3))
   '(hello world)
