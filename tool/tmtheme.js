9e3f1e663d0ff2bb432b54b9ba7ae7157fe1fab9
h3rb <hgilliland@gmail.com>
2013-03-01 21:01:02 -0500

    Update tmtheme.js
    Adding "Terminal" theme support

--- tool/tmtheme.js
+++ tool/tmtheme.js
@@ -248,6 +248,7 @@ var themes = {
     "solarized_dark": "Solarized-dark",
     "solarized_light": "Solarized-light",
     //"textmate": "Textmate (Mac Classic)",
+    "terminal": "Terminal"
     "tomorrow": "Tomorrow",
     "tomorrow_night": "Tomorrow-Night",
     "tomorrow_night_blue": "Tomorrow-Night-Blue",
