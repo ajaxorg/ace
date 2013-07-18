a69fce21f479d2f00dd1ba760ab7e490a7b75be5
nightwing <amirjanyan@gmail.com>
2013-07-18 19:35:14 +0400

    use ace static highlighter in the main site

--- doc/site/js/main.js
+++ doc/site/js/main.js
@@ -121,4 +121,31 @@ $(function() {
             }
          });
      }).trigger("hashchange");
+     
+     highlight();
 });
+
+
+
+function highlight() {
+    var highlighter = ace.require("ace/ext/static_highlight")
+    var dom = ace.require("ace/lib/dom")
+    function qsa(sel) {
+        return [].slice.call(document.querySelectorAll(sel));
+    }
+
+    qsa("code[class]").forEach(function(el) {
+        var m = el.className.match(/language-(\w+)|(javascript)/);
+        if (!m) return
+        var mode = "ace/mode/" + (m[1] || m[2]);
+        var theme = "ace/theme/xcode";
+        var data = dom.getInnerText(el).trim();
+        
+        highlighter.render(data, mode, theme, 1, true, function (highlighted) {    
+            dom.importCssString(highlighted.css, "ace_highlight");
+            el.innerHTML = highlighted.html;
+        });
+    });
+
+    
+}
