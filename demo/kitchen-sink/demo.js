d2318e99a2ae59be70b3f547f8165d8de7b7621b
nightwing <amirjanyan@gmail.com>
2013-08-05 18:51:44 +0400

    add increase/decrease fontSize commands to the demo

--- demo/kitchen-sink/demo.js
+++ demo/kitchen-sink/demo.js
@@ -177,6 +177,26 @@ env.editor.commands.addCommands([{
             editor.showKeyboardShortcuts()
         })
     }
+}, {
+    name: "increaseFontSize",
+    bindKey: "Ctrl-+",
+    exec: function(editor) {
+        var size = parseInt(editor.getFontSize(), 10) || 12;
+        editor.setFontSize(size + 1);
+    }
+}, {
+    name: "decreaseFontSize",
+    bindKey: "Ctrl+-",
+    exec: function(editor) {
+        var size = parseInt(editor.getFontSize(), 10) || 12;
+        editor.setFontSize(Math.max(size - 1 || 1));
+    }
+}, {
+    name: "resetFontSize",
+    bindKey: "Ctrl+0",
+    exec: function(editor) {
+        editor.setFontSize(12);
+    }
 }]);
 
 
