ajaxorg/ace

commit 2e6f12725bc01011c545c99c1f1c1da6065063d9

#1744: Refactor document delta handling
Refactor delta handling code to:

- Combine the "insertText" and "insertLines" delta types into a single
"insert" delta type

- Combine the "removeText" and "removeLines" delta types into a single
"remove" delta type

- Make all document mutations in a single applyDelta function.

- Add basic delta validation (more needed . . . see TODOs)

- Rework anchor logic to handle new delta types (also simplified)

- Rename "insert()" to "insertText()" and "remove()" to "removeText()"

- Rename "insertLines()" to "insertFullLines()" and "removeLines()" to
"removeFullLines()"

See related issue for more information. All tests are passing and the
changes appear functional under preliminary testing, but careful review
and testing will be necessary.

lib/ace/editor.js
@@ -601,19 +601,10 @@ var Editor = function(renderer, session) {
      *
      **/
     this.onDocumentChange = function(e) {
-        var delta = e.data;
-        var range = delta.range;
-        var lastRow;
-
-        if (range.start.row == range.end.row && delta.action != "insertLines" && delta.action != "removeLines")
-            lastRow = range.end.row;
-        else
-            lastRow = Infinity;
-        this.renderer.updateLines(range.start.row, lastRow);
-
+        this.renderer.updateLines(e.data.range.start.row, e.data.range.end.row);
         this._emit("change", e);
-
-        // update cursor because tab characters can influence the cursor position
+        
+        // update cursor because tab characters can influence the cursor position.
         this.$cursorChange();
     };
 
@@ -827,7 +818,7 @@ var Editor = function(renderer, session) {
         if (this.$readOnly)
             return;
         this._emit("paste", text);
-        this.insert(text);
+        this.insertText(text);
     };
 
 
@@ -841,7 +832,7 @@ var Editor = function(renderer, session) {
      *
      *
      **/
-    this.insert = function(text) {
+    this.insertText = function(text) {
         var session = this.session;
         var mode = session.getMode();
         var cursor = this.getCursorPosition();
@@ -887,7 +878,7 @@ var Editor = function(renderer, session) {
         var lineState = session.getState(cursor.row);
         var line = session.getLine(cursor.row);
         var shouldOutdent = mode.checkOutdent(lineState, line, text);
-        var end = session.insert(cursor, text);
+        var end = session.insertText(cursor, text);
 
         if (transform && transform.selection) {
             if (transform.selection.length == 2) { // Transform relative to the current column
@@ -906,7 +897,7 @@ var Editor = function(renderer, session) {
         if (session.getDocument().isNewLine(text)) {
             var lineIndent = mode.getNextLineIndent(lineState, line.slice(0, cursor.column), session.getTabString());
 
-            session.insert({row: cursor.row+1, column: 0}, lineIndent);
+            session.insertText({row: cursor.row+1, column: 0}, lineIndent);
         }
         if (shouldOutdent)
             mode.autoOutdent(lineState, session, cursor.row);
@@ -1279,7 +1270,7 @@ var Editor = function(renderer, session) {
         }
 
         var cursor = this.getCursorPosition();
-        this.insert("\n");
+        this.insertText("\n");
         this.moveCursorToPosition(cursor);
     };
 
@@ -1378,7 +1369,7 @@ var Editor = function(renderer, session) {
             this.selection.setSelectionRange(range);
             indentString = "\t";
         }
-        return this.insert(indentString);
+        return this.insertText(indentString);
     };
 
     /**
@@ -1516,15 +1507,7 @@ var Editor = function(renderer, session) {
      **/
     this.removeLines = function() {
         var rows = this.$getSelectedRows();
-        var range;
-        if (rows.first === 0 || rows.last+1 < this.session.getLength())
-            range = new Range(rows.first, 0, rows.last+1, 0);
-        else
-            range = new Range(
-                rows.first-1, this.session.getLine(rows.first-1).length,
-                rows.last, this.session.getLine(rows.last).length
-            );
-        this.session.remove(range);
+        this.session.removeFullLines(rows.first, rows.last);
         this.clearSelection();
     };
 
@@ -1538,7 +1521,7 @@ var Editor = function(renderer, session) {
             doc.duplicateLines(row, row);
         } else {
             var point = reverse ? range.start : range.end;
-            var endPoint = doc.insert(point, doc.getTextRange(range), false);
+            var endPoint = doc.insertText(point, doc.getTextRange(range), false);
             range.start = point;
             range.end = endPoint;
 

lib/ace/layer/gutter.js
@@ -108,8 +108,8 @@ var Gutter = function(parentEl) {
         var len = range.end.row - firstRow;
         if (len === 0) {
             // do nothing
-        } else if (delta.action == "removeText" || delta.action == "removeLines") {
-            this.$annotations.splice(firstRow, len + 1, null);
+        } else if (delta.action == "delete") {
+            this.$annotations.splice(firstRow, len, null);
         } else {
             var args = new Array(len + 1);
             args.unshift(firstRow, 1);

lib/ace/anchor.js
@@ -100,67 +100,55 @@ var Anchor = exports.Anchor = function(doc, row, column) {
      *
      **/
     this.onChange = function(e) {
-        var delta = e.data;
-        var range = delta.range;
-
-        if (range.start.row == range.end.row && range.start.row != this.row)
-            return;
-
-        if (range.start.row > this.row)
-            return;
-
-        if (range.start.row == this.row && range.start.column > this.column)
-            return;
-
-        var row = this.row;
-        var column = this.column;
-        var start = range.start;
-        var end = range.end;
-
-        if (delta.action === "insertText") {
-            if (start.row === row && start.column <= column) {
-                if (start.column === column && this.$insertRight) {
-                    // do nothing
-                } else if (start.row === end.row) {
-                    column += end.column - start.column;
-                } else {
-                    column -= start.column;
-                    row += end.row - start.row;
-                }
-            } else if (start.row !== end.row && start.row < row) {
-                row += end.row - start.row;
-            }
-        } else if (delta.action === "insertLines") {
-            if (start.row <= row) {
-                row += end.row - start.row;
-            }
-        } else if (delta.action === "removeText") {
-            if (start.row === row && start.column < column) {
-                if (end.column >= column)
-                    column = start.column;
-                else
-                    column = Math.max(0, column - (end.column - start.column));
-
-            } else if (start.row !== end.row && start.row < row) {
-                if (end.row === row)
-                    column = Math.max(0, column - end.column) + start.column;
-                row -= (end.row - start.row);
-            } else if (end.row === row) {
-                row -= end.row - start.row;
-                column = Math.max(0, column - end.column) + start.column;
-            }
-        } else if (delta.action == "removeLines") {
-            if (start.row <= row) {
-                if (end.row <= row)
-                    row -= end.row - start.row;
-                else {
-                    row = start.row;
-                    column = 0;
-                }
-            }
+        
+        function _pointsInOrder(point1, point2, equalPointsInOrder)
+        {
+            var bColIsAfter = equalPointsInOrder ? point1.column <= point2.column : point1.column < point2.column;
+            return (point1.row < point2.row) || (point1.row == point2.row && bColIsAfter);
         }
-
-        this.setPosition(row, column, true);
+                
+        function getTransformedPoint(delta, point, moveIfEqual)
+        {
+            // Get delta info.
+            var deltaIsInsert = (delta.action == 'insert')
+            var deltaRowShift = (deltaIsInsert ? 1 : -1) * (delta.range.end.row    - delta.range.start.row);
+            var deltaColShift = (deltaIsInsert ? 1 : -1) * (delta.range.end.column - delta.range.start.column);
+            var deltaStart    = delta.range.start;
+            var deltaEnd      = (deltaIsInsert ? deltaStart : delta.range.end); // Collapse insert range.
+            
+            // DELTA AFTER POINT: No change needed.
+            if (_pointsInOrder(point, deltaStart, moveIfEqual))
+            {
+                return (
+                {
+                    row: point.row,
+                    column: point.column
+                });
+            }
+            
+            // DELTA BEFORE POINT: Move point by delta shift.
+            if (_pointsInOrder(deltaEnd, point, !moveIfEqual))
+            {
+                return (
+                {
+                    row: point.row + deltaRowShift,
+                    column: point.column + (point.row == deltaEnd.row ? deltaColShift : 0)
+                });
+            }
+            
+            // DELTA ENVELOPS POINT (delete only): Move point to delta start.
+            if (delta.action != 'delete')
+                throw 'Delete action expected.';
+            return (
+            {
+                row: deltaStart.row,
+                column: deltaStart.column
+            });            
+        }
+        
+        var delta = e.data;
+        var point = getTransformedPoint(delta, {row: this.row, column: this.column}, this.$insertRight);
+        this.setPosition(point.row, point.column, true);
     };
 
     /**

lib/ace/anchor_test.js
@@ -54,7 +54,7 @@ module.exports = {
         var doc = new Document("juhu\nkinners");
         var anchor = new Anchor(doc, 1, 4);
         
-        doc.insert({row: 1, column: 1}, "123");
+        doc.insertText({row: 1, column: 1}, "123");
         assert.position(anchor.getPosition(), 1, 7);
     },
     
@@ -62,7 +62,7 @@ module.exports = {
         var doc = new Document("juhu\nkinners");
         var anchor = new Anchor(doc, 1, 4);
         
-        doc.insertLines(1, ["123", "456"]);
+        doc.insertFullLines(1, ["123", "456"]);
         assert.position(anchor.getPosition(), 3, 4);    
     },
     
@@ -70,7 +70,7 @@ module.exports = {
         var doc = new Document("juhu\nkinners");
         var anchor = new Anchor(doc, 1, 4);
         
-        doc.insertNewLine({row: 0, column: 0});
+        doc.insertLines({row: 0, column: 0}, ['', '']);
         assert.position(anchor.getPosition(), 2, 4);    
     },
     
@@ -78,7 +78,7 @@ module.exports = {
         var doc = new Document("juhu\nkinners");
         var anchor = new Anchor(doc, 1, 4);
         
-        doc.insertNewLine({row: 1, column: 2});
+        doc.insertLines({row: 1, column: 2}, ['', '']);
         assert.position(anchor.getPosition(), 2, 2);
     },
     
@@ -110,7 +110,7 @@ module.exports = {
         var doc = new Document("juhu\n1\n2\nkinners");
         var anchor = new Anchor(doc, 3, 4);
         
-        doc.removeLines(1, 2);
+        doc.removeFullLines(1, 2);
         assert.position(anchor.getPosition(), 1, 4);
     },
     
@@ -134,7 +134,7 @@ module.exports = {
         var doc = new Document("juhu\nkinners\n123");
         var anchor = new Anchor(doc, 1, 5);
         
-        doc.removeLines(1, 1);
+        doc.removeFullLines(1, 1);
         assert.position(anchor.getPosition(), 1, 0);
     },
     
@@ -173,9 +173,9 @@ module.exports = {
         var doc = new Document("juhu\nkinners\n123");
         var anchor = new Anchor(doc, 2, 4);
         
-        doc.removeLines(0, 3);
+        doc.removeFullLines(0, 3);
         assert.position(anchor.getPosition(), 0, 0);
-        doc.insertLines(0, ["a", "b", "c"]);        
+        doc.insertFullLines(0, ["a", "b", "c"]);        
         assert.position(anchor.getPosition(), 3, 0);
         assert.equal(doc.getValue(), "a\nb\nc\n");
     }

lib/ace/autocomplete.js
@@ -170,7 +170,7 @@ var Autocomplete = function() {
         "Ctrl-Down|Ctrl-End": function(editor) { editor.completer.goTo("end"); },
 
         "Esc": function(editor) { editor.completer.detach(); },
-        "Space": function(editor) { editor.completer.detach(); editor.insert(" ");},
+        "Space": function(editor) { editor.completer.detach(); editor.insertText(" ");},
         "Return": function(editor) { editor.completer.insertMatch(); },
         "Shift-Return": function(editor) { editor.completer.insertMatch(true); },
         "Tab": function(editor) { editor.completer.insertMatch(); },

lib/ace/background_tokenizer.js
@@ -183,7 +183,7 @@ var BackgroundTokenizer = function(tokenizer, editor) {
 
         if (len === 0) {
             this.lines[startRow] = null;
-        } else if (delta.action == "removeText" || delta.action == "removeLines") {
+        } else if (delta.action == "delete") {
             this.lines.splice(startRow, len + 1, null);
             this.states.splice(startRow, len + 1, null);
         } else {

lib/ace/background_tokenizer_test.js
@@ -70,7 +70,7 @@ module.exports = {
         forceTokenize(doc)
         testStates(doc, ["comment_regex_allowed", "comment_regex_allowed"])
         
-        doc.insert({row:0, column:2}, "\n*/")
+        doc.insertText({row:0, column:2}, "\n*/")
         testStates(doc, [undefined, undefined, "comment_regex_allowed"])
         
         forceTokenize(doc)

lib/ace/commands/default_commands.js
@@ -508,13 +508,13 @@ exports.commands = [{
     scrollIntoView: "selectionPart"
 }, {
     name: "insertstring",
-    exec: function(editor, str) { editor.insert(str); },
+    exec: function(editor, str) { editor.insertText(str); },
     multiSelectAction: "forEach",
     scrollIntoView: "cursor"
 }, {
     name: "inserttext",
     exec: function(editor, args) {
-        editor.insert(lang.stringRepeat(args.text  || "", args.times || 1));
+        editor.insertText(lang.stringRepeat(args.text  || "", args.times || 1));
     },
     multiSelectAction: "forEach"
 }, {

commit 026af74016a2829003a7dfa2ccde906309abd40b

Fix render bugs
Both were introduced in 2e6f12725bc01011c545c99c1f1c1da6065063d9.

lib/ace/editor.js
@@ -601,10 +601,14 @@ var Editor = function(renderer, session) {
      *
      **/
     this.onDocumentChange = function(e) {
-        this.renderer.updateLines(e.data.range.start.row, e.data.range.end.row);
+        
+        // Rerender and emit "change" event.
+        var range = e.data.range;
+        var lastRow = (range.start.row == range.end.row ? range.end.row : Infinity);
+        this.renderer.updateLines(range.start.row, lastRow);
         this._emit("change", e);
         
-        // update cursor because tab characters can influence the cursor position.
+        // Update cursor because tab characters can influence the cursor position.
         this.$cursorChange();
     };
 

lib/ace/layer/gutter.js
@@ -109,7 +109,7 @@ var Gutter = function(parentEl) {
         if (len === 0) {
             // do nothing
         } else if (delta.action == "delete") {
-            this.$annotations.splice(firstRow, len, null);
+            this.$annotations.splice(firstRow, len + 1, null);
         } else {
             var args = new Array(len + 1);
             args.unshift(firstRow, 1);