ajaxorg/ace

commit b23265ad72a85288edcf61a0d70bcda48cd28d82

allow any Range-like objects in session replace/remove

lib/ace/document.js
@@ -402,6 +402,8 @@ var Document = function(text) {
     *
     **/
     this.remove = function(range) {
+        if (!range instanceof Range)
+            range = Range.fromPoints(range.start, range.end);
         // clip to document
         range.start = this.$clipPosition(range.start);
         range.end = this.$clipPosition(range.end);
@@ -520,6 +522,8 @@ var Document = function(text) {
     *
     **/
     this.replace = function(range, text) {
+        if (!range instanceof Range)
+            range = Range.fromPoints(range.start, range.end);
         if (text.length == 0 && range.isEmpty())
             return range.start;
 

commit 3eefd3365ac477a3ece51008eefe5d9561143e07

Fixed passing non-Ranges into Document methods
Fixed regression from b23265ad: ! takes precedence over instanceof.

lib/ace/document.js
@@ -402,7 +402,7 @@ var Document = function(text) {
     *
     **/
     this.remove = function(range) {
-        if (!range instanceof Range)
+        if (!(range instanceof Range))
             range = Range.fromPoints(range.start, range.end);
         // clip to document
         range.start = this.$clipPosition(range.start);
@@ -522,7 +522,7 @@ var Document = function(text) {
     *
     **/
     this.replace = function(range, text) {
-        if (!range instanceof Range)
+        if (!(range instanceof Range))
             range = Range.fromPoints(range.start, range.end);
         if (text.length == 0 && range.isEmpty())
             return range.start;