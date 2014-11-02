399e25ceaf58c7c3d1505715f41535ef0406947a
nightwing <amirjanyan@gmail.com>
2014-11-02 15:04:55 +0400

    fix errors in imported elixir mode

--- demo/kitchen-sink/docs/elixir.ex
+++ demo/kitchen-sink/docs/elixir.ex
@@ -1,1 +1,24 @@
-TODO add a nice demo!
+defmodule HelloModule do
+  # A "Hello world" function
+  def some_fun do
+    IO.puts "Juhu Kinners!"
+  end
+  # A private function
+  defp priv do
+    is_regex ~r"""
+       This is a regex
+       spanning several
+       lines.
+    """
+    x = elem({ :a, :b, :c }, 0)  #=> :a
+  end
+end
+
+test_fun = fn(x) ->
+  cond do
+    x > 10 ->
+      :greater_than_ten
+    true ->
+      :maybe_ten
+  end
+end
