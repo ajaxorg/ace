f362d2e84abbfd76ccf189bb0cf8ac21976f84f7
nightwing <amirjanyan@gmail.com>
2014-11-05 16:56:51 +0400

    add markdown comment to elixir demo

--- demo/kitchen-sink/docs/elixir.ex
+++ demo/kitchen-sink/docs/elixir.ex
@@ -1,4 +1,22 @@
 defmodule HelloModule do
+  @moduledoc """
+    This is supposed to be `markdown`.
+    __Yes__ this is [mark](http://down.format)
+
+    # Truly
+
+    ## marked
+
+    * with lists
+    * more
+    * and more
+
+        Even.with(code)
+        blocks |> with |> samples
+
+    _Docs are first class citizens in Elixir_ (Jose Valim)
+  """
+  
   # A "Hello world" function
   def some_fun do
     IO.puts "Juhu Kinners!"
