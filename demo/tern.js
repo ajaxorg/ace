900e1bfaa70215a2b25f3bf85086f96b87c5fe4f
sevin7676 <sevin7@gmail.com>
2015-08-08 06:35:32 -0400

    partial - working w/ worker but not w/out worker
    problem: without worker requireJS is detected in worker-tern file so
    window.tern doesn't get set. To fix this I need to fix loading of tern
    files

--- demo/tern.js
+++ demo/tern.js
@@ -3,14 +3,18 @@ define(function (require, exports, module) {
     
     var mode = 'javascript'; // TODO: add html
     
+    console.warn('TODO: fix the way this is loaded....');
+    var ternWorkerPath = '../lib/ace/tern/worker-tern.js';
+    
+    // console.warn('ENTERED');
     //LEFT OFF: need to get this thing to load worker properly
-    //in the mean time, tern.html has reference to 
+    //in the mean time, tern.html has reference to
     //var config = require("ace/config");
     //console.log('config',config);
 
     //instead of using worker file.... separate into different files for worker...
-    var workerTern = require("ace/tern/worker-tern");
-    console.log('workerTern',workerTern);
+    // var workerTern = require("ace/tern/worker-tern");
+    // console.log('workerTern',workerTern);
     
 
     //create editor
@@ -38,11 +42,13 @@ define(function (require, exports, module) {
 
     editor.setOptions({
         /**
-         * Either `true` or `false` or to enable with custom options pass object that 
+         * Either `true` or `false` or to enable with custom options pass object that
          * has options for tern server: http://ternjs.net/doc/manual.html#server_api
          * If `true`, then default options will be used
          */
         enableTern: {
+            //path to worker.. should be automatic.. but having to override until I figure out how to fix this
+            workerScript: ternWorkerPath,
             /* http://ternjs.net/doc/manual.html#option_defs */
             defs: ['browser', 'ecma5'],
             /* http://ternjs.net/doc/manual.html#plugins */
@@ -61,7 +67,7 @@ define(function (require, exports, module) {
                 console.log('switchToDoc called but not defined. name=' + name + '; start=', start);
             },
             /**
-             * if passed, this function will be called once ternServer is started. 
+             * if passed, this function will be called once ternServer is started.
              * This is needed when useWorker=false because the tern source files are loaded asynchronously before the server is started.
              */
             startedCb: function () {
@@ -73,10 +79,10 @@ define(function (require, exports, module) {
          * when using tern, it takes over Ace's built in snippets support.
          * this setting affects all modes when using tern, not just javascript.
          */
-        enableSnippets: false,
+        enableSnippets: true,
         /**
-         * when using tern, Ace's basic text auto completion is enabled still by deafult. 
-         * This settings affects all modes when using tern, not just javascript. 
+         * when using tern, Ace's basic text auto completion is enabled still by deafult.
+         * This settings affects all modes when using tern, not just javascript.
          * For javascript mode the basic auto completion will be added to completion results if tern fails to find completions or if you double tab the hotkey for get completion (default is ctrl+space, so hit ctrl+space twice rapidly to include basic text completions in the result)
          */
         enableBasicAutocompletion: true,
