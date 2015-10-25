ebdf156985adc97485fc55a4515a07d2b29586b0
paladox <paladox@users.noreply.github.com>
2015-10-25 13:23:50 +0000

    Run jshint through grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'**/*.js',
+				'!node_modules/**'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
