022c0fb601243f161c3be69b4769b57e09fe48e4
nightwing <amirjanyan@gmail.com>
2016-02-15 22:33:47 +0400

    allow saving from mode_creator opened from file://

--- demo/kitchen-sink/doclist.js
+++ demo/kitchen-sink/doclist.js
@@ -185,8 +185,11 @@ function saveDoc(name, callback) {
 }
 
 function upload(url, data, callback) {
-    url = net.qualifyURL(url);
-    if (!/https?:/.test(url))
+    var absUrl = net.qualifyURL(url);
+    if (/^file:/.test(absUrl))
+        absUrl = "http://localhost:8888/" + url;
+    url = absUrl
+    if (!/^https?:/.test(url))
         return callback(new Error("Unsupported url scheme"));
     var xhr = new XMLHttpRequest();
     xhr.open("PUT", url, true);
