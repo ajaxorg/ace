0b7e3c9ab5de518c2cc71f82c052fbad2ab40efd
nightwing <amirjanyan@gmail.com>
2018-03-13 01:16:18 +0400

    include modes and workers in webpack demo

--- demo/webpack/demo.js
+++ demo/webpack/demo.js
@@ -1,22 +1,25 @@
 "use strict";
 
 import ace from '../../build'
-import {Range} from '../../build'
-console.log(Range)
+import {Range} from '../../build/'
 
-console.log(new ace.Range(0,0,1,1));
+import jsWorkerUrl from "file-loader!../../build/src-noconflict/worker-javascript";
+import jsModeUrl from "file-loader!../../build/src-noconflict/worker-javascript";
+ace.config.setModuleUrl("ace/mode/javascript_worker", jsWorkerUrl)
+ace.config.setModuleUrl("ace/mode/javascript", jsModeUrl)
 
+import {Mode as JSMode} from "../../build/src-noconflict/mode-javascript"
+
+console.log(Range.fromPoints({row: 0, column: 0}, {row: 0, column: 1}))
+console.log(new Range(0,0,0,1))
 
 var editor = ace.edit(null, {
     maxLines: 50,
     minLines: 10,
-    value: "hello world",
-    mode: "ace/mode/javascript"
+    value: "var hello = 'world'" + "\n",
+    // mode: new JSMode(),
+    mode: "ace/mode/javascript",
+    bug: 1
 })
 
 document.body.appendChild(editor.container)
-
-console.log("12345")
-
-
-
