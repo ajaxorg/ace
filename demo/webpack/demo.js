d3f4a48eecc891a45290d1d266e2b2e82dc9b0b4
nightwing <amirjanyan@gmail.com>
2019-05-29 00:17:26 +0400

    simplify loading logic

--- demo/webpack/demo.js
+++ demo/webpack/demo.js
@@ -1,15 +1,17 @@
 "use strict";
 
 // import ace
-import ace from '../../build'
+import ace from '../../lib/ace/ace'
 // import Range from ace (it is also available as ace.Range)
-import {Range, EditSession} from '../../build/'
+import {Range, EditSession} from '../../lib/ace/ace'
+
+import "../../lib/ace/webpack-resolver";
 
 // import modes that you want to include into your main bundle
-import "../../build/src-noconflict/mode-javascript";
+// import "../../build/src-noconflict/mode-javascript";
 
 // import webpack resolver to dynamically load modes, you need to install file-loader for this to work!
-import "../../build/webpack-resolver";
+
 // if you want to allow dynamic loading of only a few modules use setModuleUrl for each of them manually
 /*
 import jsWorkerUrl from "file-loader!../../build/src-noconflict/worker-javascript";
@@ -17,7 +19,7 @@ ace.config.setModuleUrl("ace/mode/javascript_worker", jsWorkerUrl)
 */
 
 var editor = ace.edit(null, {
-    maxLines: 50,
+    maxLines: 30,
     minLines: 10,
     value: "var hello = 'world'" + "\n",
     mode: "ace/mode/javascript",
