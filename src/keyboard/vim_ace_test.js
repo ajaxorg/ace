6f960393ebed764b5abdcb6e4c305fe23fe461be
mkslanc <mkslanc@gmail.com>
2022-10-29 19:06:15 +0400

    add: test for brackets highlighting in vim normal mode; test for common brackets highlighting

--- src/keyboard/vim_ace_test.js
+++ src/keyboard/vim_ace_test.js
@@ -317,6 +317,34 @@ module.exports = {
             editor.renderer.$loop._flush();
             return editor.renderer.scrollTop / editor.renderer.lineHeight;
         }
+    },
+    "test: vim normal mode brackets highlighting": function () {
+        editor.setValue("{((hello, world))}");
+        editor.focus();
+
+        var handler = editor.getKeyboardHandler();
+        var vimOffsetFunc = handler && handler.$getCursorOffsetForHighlight;
+        assert.ok(vimOffsetFunc);
+        var values = [
+            {offset: 0, anchorRow: 0, anchorColumn: 0, cursorRow: 0, cursorColumn: 0,
+                startRow: 0, startColumn: 17, endRow: 0, endColumn: 18},
+            {offset: 0, anchorRow: 0, anchorColumn: 2, cursorRow: 0, cursorColumn: 2,
+                startRow: 0, startColumn: 15, endRow: 0, endColumn: 16},
+            {offset: -1, anchorRow: 0, anchorColumn: 15, cursorRow: 0, cursorColumn: 17,
+                startRow: 0, startColumn: 1, endRow: 0, endColumn: 2},
+            {offset: 0, anchorRow: 0, anchorColumn: 17, cursorRow: 0, cursorColumn: 16,
+                startRow: 0, startColumn: 1, endRow: 0, endColumn: 2}
+        ];
+        values.forEach((el) => {
+            editor.session.selection.$setSelection(el.anchorRow, el.anchorColumn, el.cursorRow, el.cursorColumn);
+            var offset = vimOffsetFunc(editor);
+            assert.ok(offset === el.offset);
+            var ranges = editor.session.getMatchingBracketRanges({
+                row: el.cursorRow,
+                column: el.cursorColumn
+            }, offset);
+            assert.range(ranges[1], el.startRow, el.startColumn, el.endRow, el.endColumn);
+        });
     }
 };
 
