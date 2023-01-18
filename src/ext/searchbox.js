5ed424010bed78e183237121c1e4f2ffa1d35fc5
Sekedus <sekedus@gmail.com>
2023-01-18 22:54:12 +0800

    add: search/replace line break
    * highlight line break
    * search line break
    * replace line break

--- src/ext/searchbox.js
+++ src/ext/searchbox.js
@@ -263,6 +263,9 @@ var SearchBox = function(editor, range, showReplaceForm) {
                 ? editor.session.getTextRange(this.searchRange)
                 : editor.getValue();
             
+                if (editor.$search.$isMultilineSearch(editor.$search.$options))
+                    value = value.replace(/\r\n|\r|\n/g, "\n");
+            
             var offset = editor.session.doc.positionToIndex(editor.selection.anchor);
             if (this.searchRange)
                 offset -= editor.session.doc.positionToIndex(this.searchRange.start);
