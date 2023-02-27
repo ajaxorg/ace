375e61d7b1d9075eb2b3acff60c6ba845e1f99f1
tom <tom@ohhhh.me>
2023-02-27 10:53:33 -0500

    remove second "define" wrapper

--- src/mode/solidity.js
+++ src/mode/solidity.js
@@ -1,69 +1,67 @@
-define(function(require, exports, module) {
-    "use strict";
-    
-    var oop = require("../lib/oop");
-    var TextMode = require("./text").Mode;
-    var SolidityHighlightRules = require("./solidity_highlight_rules").SolidityHighlightRules;
-    var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
-    var CstyleBehaviour = require("./behaviour/cstyle").CstyleBehaviour;
-    var CStyleFoldMode = require("./folding/cstyle").FoldMode;
-    
-    var Mode = function() {
-        this.HighlightRules = SolidityHighlightRules;
-    
-        this.$outdent = new MatchingBraceOutdent();
-        this.$behaviour = new CstyleBehaviour();
-        this.foldingRules = new CStyleFoldMode();
-    };
-    oop.inherits(Mode, TextMode);
-    
-    (function() {
-    
-        this.lineCommentStart = "//";
-        this.blockComment = {start: "/*", end: "*/"};
-    
-        this.getNextLineIndent = function(state, line, tab) {
-            var indent = this.$getIndent(line);
-    
-            var tokenizedLine = this.getTokenizer().getLineTokens(line, state);
-            var tokens = tokenizedLine.tokens;
-            var endState = tokenizedLine.state;
-    
-            if (tokens.length && tokens[tokens.length-1].type == "comment") {
-                return indent;
-            }
-    
-            if (state == "start") {
-                var match = line.match(/^.*(?:\bcase\b.*\:|[\{\(\[])\s*$/);
-                if (match) {
-                    indent += tab;
-                }
-            } else if (state == "doc_comment") {
-                if (endState == "start") {
-                    return "";
-                }
-                var match = line.match(/^\s*(\/?)\*/);
-                if (match) {
-                    if (match[1]) {
-                        indent += " ";
-                    }
-                    indent += "* ";
-                }
-            }
-    
+"use strict";
+
+var oop = require("../lib/oop");
+var TextMode = require("./text").Mode;
+var SolidityHighlightRules = require("./solidity_highlight_rules").SolidityHighlightRules;
+var MatchingBraceOutdent = require("./matching_brace_outdent").MatchingBraceOutdent;
+var CstyleBehaviour = require("./behaviour/cstyle").CstyleBehaviour;
+var CStyleFoldMode = require("./folding/cstyle").FoldMode;
+
+var Mode = function() {
+    this.HighlightRules = SolidityHighlightRules;
+
+    this.$outdent = new MatchingBraceOutdent();
+    this.$behaviour = new CstyleBehaviour();
+    this.foldingRules = new CStyleFoldMode();
+};
+oop.inherits(Mode, TextMode);
+
+(function() {
+
+    this.lineCommentStart = "//";
+    this.blockComment = {start: "/*", end: "*/"};
+
+    this.getNextLineIndent = function(state, line, tab) {
+        var indent = this.$getIndent(line);
+
+        var tokenizedLine = this.getTokenizer().getLineTokens(line, state);
+        var tokens = tokenizedLine.tokens;
+        var endState = tokenizedLine.state;
+
+        if (tokens.length && tokens[tokens.length-1].type == "comment") {
             return indent;
-        };
+        }
+
+        if (state == "start") {
+            var match = line.match(/^.*(?:\bcase\b.*\:|[\{\(\[])\s*$/);
+            if (match) {
+                indent += tab;
+            }
+        } else if (state == "doc_comment") {
+            if (endState == "start") {
+                return "";
+            }
+            var match = line.match(/^\s*(\/?)\*/);
+            if (match) {
+                if (match[1]) {
+                    indent += " ";
+                }
+                indent += "* ";
+            }
+        }
+
+        return indent;
+    };
+
+    this.checkOutdent = function(state, line, input) {
+        return this.$outdent.checkOutdent(line, input);
+    };
+
+    this.autoOutdent = function(state, doc, row) {
+        this.$outdent.autoOutdent(doc, row);
+    };
     
-        this.checkOutdent = function(state, line, input) {
-            return this.$outdent.checkOutdent(line, input);
-        };
-    
-        this.autoOutdent = function(state, doc, row) {
-            this.$outdent.autoOutdent(doc, row);
-        };
-        
-        this.$id = "ace/mode/solidity";
-    }).call(Mode.prototype);
-    
-    exports.Mode = Mode;
-});
+    this.$id = "ace/mode/solidity";
+}).call(Mode.prototype);
+
+exports.Mode = Mode;
