f7756612e61af22d8d9492664f726f7e6db2a0c6
mkslanc <mkslanc@gmail.com>
2023-04-30 19:23:38 +0400

    implement special trigger characters to forcely start autocomplete

--- ace.d.ts
+++ ace.d.ts
@@ -953,6 +953,7 @@ export namespace Ace {
       callback: CompleterCallback): void;
     getDocTooltip?(item: Completion): undefined | string | Completion;
     id?: string;
+    triggerCharacters?: string[]
   }
 
   export class AceInline {
