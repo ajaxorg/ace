07bc1e87520b081261ba98ae327af6dc85bbca4e
Jonathan <vanillajonathan@users.noreply.github.com>
2023-09-09 19:39:51 +0200

    Update prql_highlight_rules.js

--- src/mode/prql_highlight_rules.js
+++ src/mode/prql_highlight_rules.js
@@ -6,7 +6,7 @@ var oop = require("../lib/oop");
 var TextHighlightRules = require("./text_highlight_rules").TextHighlightRules;
 
 var PrqlHighlightRules = function() {
-    var builtinFunctions = "min|max|sum|average|stddev|every|any|concat_array|count|" +
+    const builtinFunctions = "min|max|sum|average|stddev|every|any|concat_array|count|" +
     "lag|lead|first|last|rank|rank_dense|row_number|" +
     "round|as|in|" +
     "tuple_every|tuple_map|tuple_zip|_eq|_is_null|" +
@@ -14,7 +14,7 @@ var PrqlHighlightRules = function() {
     "lower|upper|" +
     "read_parquet|read_csv";
 
-    var builtinTypes = [
+    const builtinTypes = [
         "bool",
         "int",
         "int8",
@@ -60,7 +60,7 @@ var PrqlHighlightRules = function() {
             regex : /[,`]/
         }, {
             token : "constant.language",
-            regex : "^" + identifierRe + "*",
+            regex : "^" + identifierRe + "*"
         }, {
             token : keywordMapper,
             regex : "[\\w\\xff-\\u218e\\u2455-\\uffff]+\\b"
