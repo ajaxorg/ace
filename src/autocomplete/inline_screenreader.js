5c2d9ac64386dcb06bbb7a0fd7ab177dab61e326
Alice <t.koreman@hotmail.nl>
2023-09-24 20:58:34 +0200

    change to use aria-details

--- src/autocomplete/inline_screenreader.js
+++ src/autocomplete/inline_screenreader.js
@@ -4,25 +4,34 @@ class AceInlineScreenReader {
 
         this.screenReaderDiv = document.createElement("div");
         this.screenReaderDiv.classList.add("ace_screenreader_only");
-        this.screenReaderDiv.setAttribute("id", "ace-inline-screenreader");
         this.editor.container.appendChild(this.screenReaderDiv);
     }
 
     setScreenReaderContent(content) {
+        if (!this.popup) {
+            this.popup = this.editor.completer.popup;
+
+            this.popup.renderer.on("afterRender", function() {
+                let row = this.popup.getRow();
+                let t = this.popup.renderer.$textLayer;
+                let selected = t.element.childNodes[row - t.config.firstRow];
+                if (selected) {
+                    let idString = "";
+                    for (let lineIndex = 0; lineIndex < this._lines.length; lineIndex++) {
+                        idString += `ace-inline-screenreader-line-${lineIndex} `
+                    }
+                    selected.setAttribute("aria-details", idString);      
+                };
+            }.bind(this));
+        };
+
         // Remove all children of the div
         while (this.screenReaderDiv.firstChild) {
             this.screenReaderDiv.removeChild(this.screenReaderDiv.firstChild);
         }
-
-        const codeElement = AceInlineScreenReader.createCodeBlock(content);
+        this._lines = content.split(/\r\n|\r|\n/);
+        const codeElement = this.createCodeBlock();
         this.screenReaderDiv.appendChild(codeElement);
-
-        const popup = editor.completer.popup;
-
-        var popupTextLayer = popup.renderer.$textLayer;
-        var row = popup.getRow();
-        var selectedPopupElement = popupTextLayer.element.childNodes[row - popupTextLayer.config.firstRow];
-        selectedPopupElement.setAttribute("aria-describedby", "ace-inline-screenreader");
     }
 
     destroy() {
@@ -34,15 +43,13 @@ class AceInlineScreenReader {
         this.screenReaderDiv.remove();
     }
 
-    static createCodeBlock(content) {
+    createCodeBlock() {
         const container = document.createElement("pre");
-        this._lines = content.split(/\r\n|\r|\n/);
+        container.setAttribute("id", "ace-inline-screenreader");
 
         for (let lineIndex = 0; lineIndex < this._lines.length; lineIndex++) {
-            const lineElement = document.createElement("div");
-            lineElement.className = "ace_line";
-            
             const codeElement = document.createElement("code");
+            codeElement.setAttribute("id", `ace-inline-screenreader-line-${lineIndex}`);
             const line = document.createTextNode(this._lines[lineIndex]);
 
             codeElement.appendChild(line);
