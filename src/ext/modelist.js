4fff99e320539a1472ce0512cd1fd3e5fc7f2ca6
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2023-10-16 07:29:53 +0000

    feat: add nasal language (#5342)
    * feat: add nasal language
    
    * fix: update nasal mode
    
    * fix: fix linter errors
    
    * feat: add nasal tokens

--- src/ext/modelist.js
+++ src/ext/modelist.js
@@ -150,6 +150,7 @@ var supportedModes = {
     MIXAL:       ["mixal"],
     MUSHCode:    ["mc|mush"],
     MySQL:       ["mysql"],
+    Nasal:       ["nas"],
     Nginx:       ["nginx|conf"],
     Nim:         ["nim"],
     Nix:         ["nix"],
