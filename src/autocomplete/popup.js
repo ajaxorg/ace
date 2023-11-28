6100090c01b7f82df52af134ac4a0c96c8c6f5a6
Alice Koreman <a.koreman@outlook.com>
2023-11-28 11:18:46 +0100

    set aria-selected

--- src/autocomplete/popup.js
+++ src/autocomplete/popup.js
@@ -129,6 +129,7 @@ class AcePopup {
             if (selected !== t.selectedNode && t.selectedNode) {
                 dom.removeCssClass(t.selectedNode, "ace_selected");
                 el.removeAttribute("aria-activedescendant");
+                selected.removeAttribute("aria-selected");
                 t.selectedNode.removeAttribute("id");
             }
             t.selectedNode = selected;
@@ -144,6 +145,7 @@ class AcePopup {
                 selected.setAttribute("aria-setsize", popup.data.length);
                 selected.setAttribute("aria-posinset", row+1);
                 selected.setAttribute("aria-describedby", "doc-tooltip");
+                selected.setAttribute("aria-selected", "true");
             }
         });
         var hideHoverMarker = function() { setHoverMarker(-1); };
