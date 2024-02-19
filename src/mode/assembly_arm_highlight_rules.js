2731f962b4ea6a20610e05025c525bb6079891c8
mitch <mitchhudson2002@gmail.com>
2024-02-18 22:54:07 -0600

    Updated with register aliases

--- src/mode/assembly_arm_highlight_rules.js
+++ src/mode/assembly_arm_highlight_rules.js
@@ -16,7 +16,7 @@ var AssemblyARMHighlightRules = function() {
             },
             { 
                 token: 'variable.parameter.register.assembly',
-                regex: '\\b(?:r0|r1|r2|r3|r4|r5|r6|r7|r8|r9|r10|r11|r12|r13|r14|lr)\\b',
+                regex: '\\b(?:r0|r1|r2|r3|r4|r5|r6|r7|r8|r9|r10|r11|r12|r13|r14|r15|fp|ip|sp|lr|pc|cpsr)\\b',
                 caseInsensitive: true 
             },
             { 
