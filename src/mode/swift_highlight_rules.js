74854c852873c4ecabb0d5190af09f5a30eddf55
Oyku Yilmaz <oyku@amazon.com>
2024-03-21 13:15:35 +0100

    fix: include HighlightRules export for backwards compatibility

--- src/mode/swift_highlight_rules.js
+++ src/mode/swift_highlight_rules.js
@@ -166,4 +166,5 @@ var SwiftHighlightRules = function() {
 
 oop.inherits(SwiftHighlightRules, TextHighlightRules);
 
+exports.HighlightRules = SwiftHighlightRules;
 exports.SwiftHighlightRules = SwiftHighlightRules;
