7869158657b5ea7b6e1b227b6a68979bca574263
Bagus Dwi Utama <dwiutamabagus@gmail.com>
2024-04-13 21:51:40 +0700

    Fix Incorrect Module Reference for TypeScriptHighlightRules (#5531)
    * fix module reference for tsx highlight mode
    
    * fix formatting
    
    ---------
    
    Co-authored-by: nightwing <amirjanyan@gmail.com>

--- src/mode/tsx_highlight_rules.js
+++ src/mode/tsx_highlight_rules.js
@@ -1,13 +1,12 @@
 "use strict";
 
 var oop = require("../lib/oop");
-var TypeScriptHighlightRules = require("./javascript_highlight_rules").TypeScriptHighlightRules;
-
+var TypeScriptHighlightRules = require("./typescript_highlight_rules").TypeScriptHighlightRules;
 
 var TsxHighlightRules = function () {
-  TypeScriptHighlightRules.call(this, {
-    jsx: true
-  });
+    TypeScriptHighlightRules.call(this, {
+      jsx: true
+    });
 };
 oop.inherits(TsxHighlightRules, TypeScriptHighlightRules);
 
