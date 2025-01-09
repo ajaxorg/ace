2c8bf916d75b086b958c205896bc2adc1747d241
Joan Perals <jperals@protonmail.com>
2025-01-09 13:45:05 +0100

    fix: Remove unnecesary `typeof` from type imports in declarations (#5715)

--- ace.d.ts
+++ ace.d.ts
@@ -36,8 +36,8 @@ declare module "ace-code" {
         type DragdropHandler = import("ace-code/src/mouse/dragdrop_handler").DragdropHandler;
         type AppConfig = import("ace-code/src/lib/app_config").AppConfig;
         type Config = typeof import("ace-code/src/config");
-        type GutterTooltip = typeof import("ace-code/src/mouse/default_gutter_handler").GutterTooltip;
-        type GutterKeyboardEvent = typeof import("ace-code/src/keyboard/gutter_handler").GutterKeyboardEvent;
+        type GutterTooltip = import("ace-code/src/mouse/default_gutter_handler").GutterTooltip;
+        type GutterKeyboardEvent = import("ace-code/src/keyboard/gutter_handler").GutterKeyboardEvent;
         type AfterLoadCallback = (err: Error | null, module: unknown) => void;
         type LoaderFunction = (moduleName: string, afterLoad: AfterLoadCallback) => void;
         export interface ConfigOptions {
