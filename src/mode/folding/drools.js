78ad9c73c2d5784740c61f1973c3cfeb008d7e72
Hugh Samson <samson.hugh@gmail.com>
2025-03-25 16:50:57 +0000

    Update drools folding to include 'global' and 'import' (#5775)
    Co-authored-by: Hugh Samson <hugh.samson@tracsis.com>

--- src/mode/folding/drools.js
+++ src/mode/folding/drools.js
@@ -8,18 +8,36 @@ var TokenIterator = require("../../token_iterator").TokenIterator;
 var FoldMode = exports.FoldMode = function() {};
 oop.inherits(FoldMode, BaseFoldMode);
 
-(function() {
+(function () {
 
     // regular expressions that identify starting and stopping points
-    this.foldingStartMarker = /\b(rule|declare|query|when|then)\b/; 
+    this.foldingStartMarker = /\b(rule|declare|query|when|then)\b/;
     this.foldingStopMarker = /\bend\b/;
+    this.importRegex = /^import /;
+    this.globalRegex = /^global /;
+    this.getBaseFoldWidget = this.getFoldWidget;
 
-    this.getFoldWidgetRange = function(session, foldStyle, row) {
+    this.getFoldWidget = function (session, foldStyle, row) {
+        if (foldStyle === "markbegin") {
+            var line = session.getLine(row);
+            if (this.importRegex.test(line)) {
+                if (row === 0 || !this.importRegex.test(session.getLine(row - 1)))
+                    return "start";
+            }
+            if (this.globalRegex.test(line)) {
+                if (row === 0 || !this.globalRegex.test(session.getLine(row - 1)))
+                    return "start";
+            }
+        }
+
+        return this.getBaseFoldWidget(session, foldStyle, row);
+    };
+
+    this.getFoldWidgetRange = function (session, foldStyle, row) {
         var line = session.getLine(row);
         var match = line.match(this.foldingStartMarker);
-        if (match) {
-            var i = match.index;
 
+        if (match) {
             if (match[1]) {
                 var position = {row: row, column: line.length};
                 var iterator = new TokenIterator(session, position.row, position.column);
@@ -29,7 +47,7 @@ oop.inherits(FoldMode, BaseFoldMode);
                     seek = "then";
                 }
                 while (token) {
-                    if (token.value == seek) { 
+                    if (token.value == seek) {
                         return Range.fromPoints(position ,{
                             row: iterator.getCurrentTokenRow(),
                             column: iterator.getCurrentTokenColumn()
@@ -40,7 +58,38 @@ oop.inherits(FoldMode, BaseFoldMode);
             }
 
         }
+        match = line.match(this.importRegex);
+        if (match) {
+            return getMatchedFoldRange(session, this.importRegex, match, row);
+        }
+        match = line.match(this.globalRegex);
+        if (match) {
+            return getMatchedFoldRange(session, this.globalRegex, match, row);
+        }
         // test each line, and return a range of segments to collapse
     };
 
 }).call(FoldMode.prototype);
+
+function getMatchedFoldRange(session, regex, match, row) {
+    let startColumn = match[0].length;
+    let maxRow = session.getLength();
+    let startRow = row;
+    let endRow = row;
+
+    while (++row < maxRow) {
+        let line = session.getLine(row);
+        if (line.match(/^\s*$/))
+            continue;
+
+        if (!line.match(regex))
+            break;
+
+        endRow = row;
+    }
+
+    if (endRow > startRow) {
+        let endColumn = session.getLine(endRow).length;
+        return new Range(startRow, startColumn, endRow, endColumn);
+    }
+}
