d309daaf0d409e6ed965835b56128c009a1c305f
maria-raluca-st <mralucas@amazon.com>
2025-12-02 17:36:01 +0100

    release v1.43.5

--- src/config.js
+++ src/config.js
@@ -197,6 +197,6 @@ var reportErrorIfPathIsNotConfigured = function() {
     }
 };
 
-exports.version = "1.43.4";
+exports.version = "1.43.5";
 
 
--- types/ace-modules.d.ts
+++ types/ace-modules.d.ts
@@ -375,7 +375,7 @@ declare module "ace-code/src/config" {
             string
         ], onLoad: (module: any) => void) => void;
         setModuleLoader: (moduleName: any, onLoad: any) => void;
-        version: "1.43.4";
+        version: "1.43.5";
     };
     export = _exports;
 }
